8ac62c618ad3d259d889f8cb3a28842d2a0b7725
Lucius <lingleix@hotmail.com>
2021-12-04 00:08:31 +0800

    Remove svg

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
-// const path = require("path");
-
-// const { resolve } = require("core-js/fn/promise");
-
-
 module.exports = {
   css: {
     loaderOptions: {
@@ -18,21 +13,3 @@ module.exports = {
   }
 };
 
-// module.exports = {
-//   chainWebpack: config => {
-//     config.module.rules.delete("svg")
-//     config.module
-//       .rule("svg-sprite-loader")
-//       .test(/\.svg$/)
-//       .include
-//       .add(path.join(__dirname, "./src/assets/icon"))
-//       .end()
-//       .use("svg-sprite-loader")
-//       .loader("svg-sprite-loader")
-//       .options({ symbolId: "[name]" })
-//     config.module
-//       .rule("images")
-//     .exclude.add(resolve("src/assets/icon"))
-//   },
-// }
-// };
