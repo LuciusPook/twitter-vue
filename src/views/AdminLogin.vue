83f7d9491b0a978d8cb6a77333e43cddba0197d2
Lucius <lingleix@hotmail.com>
2021-12-04 00:05:10 +0800

    feat: Fetch data from API

--- src/views/AdminLogin.vue
+++ src/views/AdminLogin.vue
@@ -1,17 +1,110 @@
 <template>
-  <AccountForm />
+  <div class="container">
+    <img src="https://i.ibb.co/WD1YSyW/Logo.jpg" alt="Logo" />
+    <h2 class="form-title">後台登入</h2>
+    <AccountForm
+      :Email="email"
+      :Password="password"
+      @after-submit="handleAfterSubmit"
+      :is-processing="isProcessing"
+    />
+    <div class="form-link">
+      <router-link to="/login">前台登入</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 import AccountForm from "./../components/AccountForm";
-// import Spinner from "./../components/Spinner";
+import authorizationAPI from "./../apis/authorization";
+import { Toast } from "./../utils/helpers";
 
 export default {
   components: {
     AccountForm,
   },
   data() {
-    return {};
+    return {
+      email: "",
+      password: "",
+      isProcessing: false,
+    };
+  },
+  methods: {
+    async handleAfterSubmit(data) {
+      const { email, password } = data;
+
+      try {
+        if (!email || !password) {
+          Toast.fire({
+            icon: "warning",
+            title: "請填入帳號和密碼",
+          });
+          return;
+        }
+
+        this.isProcessing = true;
+
+        const { data } = await authorizationAPI.signIn({
+          email,
+          password,
+        });
+
+        if (data.status === "error" || data.user.role !== "Admin") {
+          throw new Error(data.message);
+        }
+
+        localStorage.setItem("token", data.token);
+
+        this.$store.commit("setCurrentUser", data.user);
+
+        this.$router.push("/admin/tweets");
+      } catch (error) {
+        this.password = "";
+        this.isProcessing = false;
+
+        Toast.fire({
+          icon: "warning",
+          title: "請確認您輸入正確的賬號密碼",
+        });
+      }
+    },
   },
 };
 </script>
+
+
+<style lang="scss" scoped>
+div.container {
+  text-align: center;
+  padding-top: 4rem;
+  flex: 1;
+}
+
+img {
+  width: 40px;
+  height: 40px;
+  margin: 0 auto;
+}
+
+h2 {
+  margin: 25px 0 8px;
+}
+
+a {
+  font-weight: bold;
+  font-size: 18px;
+  margin: 5px;
+  color: #0099ff;
+  text-decoration: underline;
+  transition: all 0.3s ease-in-out;
+}
+
+a:hover {
+  color: #0051ff;
+}
+
+.form-link {
+  margin-left: 29rem;
+}
+</style>
