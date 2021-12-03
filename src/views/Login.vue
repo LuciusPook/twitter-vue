5a0eb30b094e06cdc0f168a65182c227a4744b27
Lucius <lingleix@hotmail.com>
2021-12-04 00:03:25 +0800

    feat: Fetch data from API

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,25 @@
 <template>
-  <AccountForm
-    :Account="account"
-    :Password="password"
-    @after-submit="handleAfterSubmit"
-    :is-processing="isProcessing"
-  />
+  <div class="container">
+    <img src="https://i.ibb.co/WD1YSyW/Logo.jpg" alt="Logo" />
+    <h2 class="form-title">登入 Alphitter</h2>
+    <AccountForm
+      :Email="email"
+      :Password="password"
+      @after-submit="handleAfterSubmit"
+      :is-processing="isProcessing"
+    />
+    <div class="form-link">
+      <router-link to="/register">註冊 Alphitter</router-link>
+      <span></span>
+      <router-link to="/admin/login">後台登入</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 import AccountForm from "./../components/AccountForm";
 import authorizationAPI from "./../apis/authorization";
 import { Toast } from "./../utils/helpers";
-// import Spinner from "./../components/Spinner";
 
 export default {
   components: {
@@ -19,15 +27,17 @@ export default {
   },
   data() {
     return {
-      account: "",
+      email: "",
       password: "",
       isProcessing: false,
     };
   },
   methods: {
-    async handleAfterSubmit() {
+    async handleAfterSubmit(data) {
+      const { email, password } = data;
+
       try {
-        if (!this.account || !this.password) {
+        if (!email || !password) {
           Toast.fire({
             icon: "warning",
             title: "請填入帳號和密碼",
@@ -38,22 +48,17 @@ export default {
         this.isProcessing = true;
 
         const { data } = await authorizationAPI.signIn({
-          account: this.account,
-          password: this.password,
-         
+          email,
+          password,
         });
 
-        if (data.status === "error") {
-          throw new Error(data.message);
-        }
-
-        if (data.user.role === "admin") {
+        if (data.status === "error" || data.user.role === "Admin") {
           throw new Error(data.message);
         }
 
         localStorage.setItem("token", data.token);
 
-        this.$store.commit("setCurrentUser, data.user");
+        this.$store.commit("setCurrentUser", data.user);
 
         this.$router.push("/main");
       } catch (error) {
@@ -69,3 +74,47 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+div.container {
+  text-align: center;
+  padding-top: 4rem;
+  flex: 1;
+}
+
+img {
+  width: 40px;
+  height: 40px;
+  margin: 0 auto;
+}
+
+h2 {
+  margin: 25px 0 8px;
+}
+
+a {
+  font-weight: bold;
+  font-size: 18px;
+  margin: 5px;
+  color: #0099ff;
+  text-decoration: underline;
+  transition: all 0.3s ease-in-out;
+}
+
+a:hover {
+  color: #0051ff;
+}
+
+.form-link {
+  margin-left: 21rem;
+}
+
+span {
+  display: inline-block;
+  width: 4px;
+  height: 4px;
+  margin-bottom: 2px;
+  background: #0099ff;
+  border-radius: 50%;
+}
+</style>
