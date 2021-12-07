daf4648eb70c3d89313740fb9799d0541664eb68
Lucius <lingleix@hotmail.com>
2021-12-07 17:47:11 +0800

    feat: fetch & modify api

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -2,10 +2,15 @@ import { apiHelper } from '../utils/helpers'
 
 
 export default {
+  getUsers() {
+    return apiHelper.get('/admin/users')
+  },
+
   getTweets() {
     return apiHelper.get('/admin/tweets')
   },
-  getUsers() {
-    return apiHelper.get('/admin/users')
+
+  deleteTweet({ tweetsId }) {
+    return apiHelper.get(`/admin/tweets/${tweetsId}`)
   },
 }
