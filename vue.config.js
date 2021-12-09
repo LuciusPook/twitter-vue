e3b6e3060b541dc33374c5b4e0d047ab6b3b6315
JackHuo <cji4dk40@gmail.com>
2021-12-10 07:19:30 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,21 @@
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       sass: {
-//         prependData: ` 
-//         @import "@/scss/_color.scss"; 
-//         @import "@/scss/_reset.scss"; 
-//         @import "@/scss/_extend.scss"; 
-//         @import "@/scss/_mixin.scss"; 
-//         `
-//       }
-//     }
-//   },
-//   publicPath: process.env.NODE_ENV === 'production'
-//   ? '/simple-twitter-vue-2021/'
-//   : '/'
-// };
-
+// ./vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/simple-twitter-vue-2021/'
     : '/'
+}
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: ` 
+        @import "@/scss/_color.scss"; 
+        @import "@/scss/_reset.scss"; 
+        @import "@/scss/_extend.scss"; 
+        @import "@/scss/_mixin.scss"; 
+        `
+      }
+    }
+  },
 };
+
