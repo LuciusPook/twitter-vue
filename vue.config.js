97992346569ec2556257e6e195f37b578f81490e
JackHuo <cji4dk40@gmail.com>
2021-12-10 07:55:42 +0800

    commit for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
-// ./vue.config.js
+./vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/twitter-vue/'
     : '/'
 }
-module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: ` 
-        @import "@/scss/_color.scss"; 
-        @import "@/scss/_reset.scss"; 
-        @import "@/scss/_extend.scss"; 
-        @import "@/scss/_mixin.scss"; 
-        `
-      }
-    }
-  },
-};
+// module.exports = {
+//   css: {
+//     loaderOptions: {
+//       sass: {
+//         prependData: ` 
+//         @import "@/scss/_color.scss"; 
+//         @import "@/scss/_reset.scss"; 
+//         @import "@/scss/_extend.scss"; 
+//         @import "@/scss/_mixin.scss"; 
+//         `
+//       }
+//     }
+//   },
+// };
 
